dc0300cac3b3dac599dd186a834d222d3f51e7ca
Anthony R. Spyker <anthonyspyker@gmail.com>
2021-03-04 11:36:28 -0500

    basic file structure complete

---
+++ config/orm.js
@@ -0,0 +1,9 @@
+const connection = require('/connection');
+
+const selectAll = () => {};
+
+const insertAll = () => {};
+
+const updateOne = () => {};
+
+module.exports = orm;
---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("That's a burger!", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Where is that burger!?", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hey! Found that burger!", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Where did that burger go?", true)
