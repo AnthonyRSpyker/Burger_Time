a41bef0a3e18eedbecfdf8a72fb4fd7a7e65afd3
Anthony R. Spyker <anthonyspyker@gmail.com>
2021-03-10 10:56:03 -0500

    added a way to hide password and made some small style changes to html and css

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
+require('dotenv').config();
 
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
   
-  password: '',
+  password: process.env.password,
   database: 'burgers_db',
 });
 
